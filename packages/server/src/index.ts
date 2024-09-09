c671a6a8038295f03b4d6eb2d0391ca120761640
Tyee <flin00806@gmail.com>
2024-09-09 16:43:55 +0800

    feat(server): 配置跨域CORS插件

--- packages/server/src/index.ts
+++ packages/server/src/index.ts
@@ -1,9 +1,11 @@
 import Elysia from 'elysia'
 import { TableService } from './services/table'
 import { MermaidService } from './services'
+import cors from '@elysiajs/cors'
 
 const port = 4444
 new Elysia()
+    .use(cors())
     .use(TableService)
     .use(MermaidService)
     .listen(port, () => {
