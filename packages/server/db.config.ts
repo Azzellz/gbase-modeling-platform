4485eb4ff44efe4561274ee1a1ecb0414e6d1f4e
Tyee <flin00806@gmail.com>
2024-09-09 17:47:04 +0800

    feat(server): 使用环境变量配置db.config.ts

--- packages/server/db.config.ts
+++ packages/server/db.config.ts
@@ -1,11 +1,11 @@
 export default {
     host: 'localhost',
     port: 5432,
-    username: 'test',
+    username: 'gbase',
     database: 'testdb',
-    password: 'Abc123!@',
+    password: process.env['GBASE_PASSWORD'],
     gbase: {
         // gbase数据库的安装路径
-        path: ''
+        path: '/home/gbase/gbase8c_pkg'
     }
 }
