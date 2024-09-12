e8a0763aaf3c812b90cdc645e72c84d2d0588846
Tyee <flin00806@gmail.com>
2024-09-12 15:54:40 +0800

    feat(server): 给后端退出的情况注册数据库断开连接的事件

--- packages/server/src/db/engine.ts
+++ packages/server/src/db/engine.ts
@@ -8,14 +8,41 @@ const client = new OpenGauss()
 client.connect(config).then(() => {
     console.log('数据库连接成功!')
 })
-process.on('exit', () => {
+function disconnect() {
     try {
         client.disconnect()
         console.log('成功断开数据库连接')
     } catch (error) {
         console.error('断开数据库连接失败: ' + error)
     }
-})
+}
+// 正常退出事件
+process.on('exit', disconnect);
+
+// 未捕获的异常事件
+process.on('uncaughtException', (err) => {
+    console.error('There was an uncaught exception:', err);
+    // 如果你需要强制退出：
+    process.exit(1);
+});
+
+// 未处理的Promise拒绝事件
+process.on('unhandledRejection', (reason, promise) => {
+    console.error('Unhandled Rejection at:', promise, 'reason:', reason);
+    // 在这里处理未处理的Promise拒绝
+});
+
+// 捕获 SIGINT (Ctrl+C)
+process.on('SIGINT', () => {
+    console.log('Received SIGINT. Exiting gracefully...');
+    process.exit(0);
+});
+
+// 捕获 SIGTERM (通常用于终止进程)
+process.on('SIGTERM', () => {
+    console.log('Received SIGTERM. Exiting gracefully...');
+    process.exit(0);
+});
 
 //#endregion
 
