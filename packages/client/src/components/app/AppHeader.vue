6e4952d9b139f779cc5fb874b7666add5090f890
Tyee <flin00806@gmail.com>
2024-09-12 11:00:50 +0800

    feat(client): 更好看的顶部路由导航

--- packages/client/src/components/app/AppHeader.vue
+++ packages/client/src/components/app/AppHeader.vue
@@ -1,12 +1,20 @@
 <template>
     <nav class="px-5 h-16 flex items-center justify-between shadow-xl font-bold">
         <span class="text-lg min-w-60">
-            <RouterLink to="/">南大通用GBase8c建模平台</RouterLink>
+            <RouterLink class="router-link" active-class="router-link-active" to="/">
+                南大通用GBase8c建模平台
+            </RouterLink>
         </span>
         <div class="flex gap-10 mr-10">
-            <RouterLink to="/db">数据库</RouterLink>
-            <RouterLink to="/modeling">建模</RouterLink>
-            <RouterLink to="/erd">关系图</RouterLink>
+            <RouterLink class="router-link" active-class="router-link-active" to="/db">
+                数据库
+            </RouterLink>
+            <RouterLink class="router-link" active-class="router-link-active" to="/modeling">
+                建模
+            </RouterLink>
+            <RouterLink class="router-link" active-class="router-link-active" to="/erd">
+                关系图
+            </RouterLink>
         </div>
     </nav>
 </template>
--- packages/client/uno.config.ts
+++ packages/client/uno.config.ts
@@ -3,4 +3,12 @@ import { defineConfig } from 'unocss'
 
 export default defineConfig({
     // ...UnoCSS options
+    shortcuts: {
+        tip: 'text-sm text-gray font-bold',
+        'text-primary': 'text-green-600', // 主题色
+        'border-bottom-primary': 'border-b-solid border-b-1 border-b-green-600',
+        'router-link': 'transition-all hover:text-primary hover:scale-110 hover:border-bottom-primary',
+        'router-link-active': 'transition-all text-primary scale-110 border-bottom-primary',
+        'router-link-no-border': 'transition-all hover:text-primary hover:scale-110 ',
+    }
 })
