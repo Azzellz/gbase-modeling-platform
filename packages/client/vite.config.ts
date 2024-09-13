5de4da927a129cd2bc935d154e0f3a109da8b386
Tyee <flin00806@gmail.com>
2024-09-13 16:03:59 +0800

    fix(client): 修复vite.config.ts中别名的问题

--- packages/client/vite.config.ts
+++ packages/client/vite.config.ts
@@ -13,7 +13,8 @@ export default defineConfig({
     plugins: [vue(), vueJsx(), vueDevTools(), UnoCSS()],
     resolve: {
         alias: {
-            '@': fileURLToPath(new URL('./src', import.meta.url))
+            '@': fileURLToPath(new URL('./src', import.meta.url)),
+            '@root/config': fileURLToPath(new URL('../../config.ts', import.meta.url))
         }
     }
 })
